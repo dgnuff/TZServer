0e4917e7bcf758d01618dc51713824c7aba0dea0
David Goodenough <dgnuff@gmail.com>
2024-11-28 17:06:02 -0800

    Initial commit

---
+++ tzinfo.js
@@ -0,0 +1,31 @@
+module.exports =
+{
+    tzInfo:
+    [
+        { "name": "pacific/honolulu",      "offset":  600, "dst_month":  0, "dst_date":  0, "dst_dow": 0, "dst_hour": 0, "std_month":  0, "std_date":  0, "std_dow": 0, "std_hour": 0 },
+        { "name": "america/anchorage",     "offset": -540, "dst_month":  3, "dst_date":  8, "dst_dow": 0, "dst_hour": 2, "std_month": 11, "std_date":  1, "std_dow": 0, "std_hour": 2 },
+        { "name": "america/los_angeles",   "offset": -480, "dst_month":  3, "dst_date":  8, "dst_dow": 0, "dst_hour": 2, "std_month": 11, "std_date":  1, "std_dow": 0, "std_hour": 2 },
+        { "name": "america/phoenix",       "offset": -420, "dst_month":  0, "dst_date":  0, "dst_dow": 0, "dst_hour": 0, "std_month":  0, "std_date":  0, "std_dow": 0, "std_hour": 0 },
+        { "name": "america/denver",        "offset": -420, "dst_month":  3, "dst_date":  8, "dst_dow": 0, "dst_hour": 2, "std_month": 11, "std_date":  1, "std_dow": 0, "std_hour": 2 },
+        { "name": "america/chicago",       "offset": -360, "dst_month":  3, "dst_date":  8, "dst_dow": 0, "dst_hour": 2, "std_month": 11, "std_date":  1, "std_dow": 0, "std_hour": 2 },
+        { "name": "america/new_york",      "offset": -300, "dst_month":  3, "dst_date":  8, "dst_dow": 0, "dst_hour": 2, "std_month": 11, "std_date":  1, "std_dow": 0, "std_hour": 2 },
+        { "name": "america/sao_paulo",     "offset": -180, "dst_month":  0, "dst_date":  0, "dst_dow": 0, "dst_hour": 0, "std_month":  0, "std_date":  0, "std_dow": 0, "std_hour": 0 },
+        { "name": "Atlantic/Azores",       "offset" : -60, "dst_month":  3, "dst_date": -7, "dst_dow": 0, "dst_hour": 0, "std_month": 10, "std_date": -7, "std_dow": 0, "std_hour": 1 },
+        { "name": "europe/london",         "offset":    0, "dst_month":  3, "dst_date": -7, "dst_dow": 0, "dst_hour": 1, "std_month": 10, "std_date": -7, "std_dow": 0, "std_hour": 2 },
+        { "name": "europe/lisbon",         "offset":    0, "dst_month":  3, "dst_date": -7, "dst_dow": 0, "dst_hour": 1, "std_month": 10, "std_date": -7, "std_dow": 0, "std_hour": 2 },
+        { "name": "europe/berlin",         "offset":   60, "dst_month":  3, "dst_date": -7, "dst_dow": 0, "dst_hour": 2, "std_month": 10, "std_date": -7, "std_dow": 0, "std_hour": 3 },
+        { "name": "europe/athens",         "offset":  120, "dst_month":  3, "dst_date": -7, "dst_dow": 0, "dst_hour": 3, "std_month": 10, "std_date": -7, "std_dow": 0, "std_hour": 4 },
+        { "name": "africa/johannesburg",   "offset":  120, "dst_month":  0, "dst_date":  0, "dst_dow": 0, "dst_hour": 0, "std_month":  0, "std_date":  0, "std_dow": 0, "std_hour": 0 },
+        { "name": "asia/jerusalem",        "offset":  120, "dst_month":  3, "dst_date": -9, "dst_dow": 5, "dst_hour": 2, "std_month": 10, "std_date": -7, "std_dow": 0, "std_hour": 2 },
+        { "name": "Asia/Kolkata",          "offset" : 330, "dst_month":  0, "dst_date":  0, "dst_dow": 0, "dst_hour": 0, "std_month" : 0, "std_date" : 0, "std_dow": 0, "std_hour": 0 },
+        { "name": "Asia/Hong_Kong",        "offset" : 480, "dst_month":  0, "dst_date":  0, "dst_dow": 0, "dst_hour": 0, "std_month" : 0, "std_date" : 0, "std_dow": 0, "std_hour": 0 },
+        { "name": "Asia/Tokyo",            "offset" : 540, "dst_month":  0, "dst_date":  0, "dst_dow": 0, "dst_hour": 0, "std_month" : 0, "std_date" : 0, "std_dow": 0, "std_hour": 0 },
+        { "name": "Asia/Seoul",            "offset" : 540, "dst_month":  0, "dst_date":  0, "dst_dow": 0, "dst_hour": 0, "std_month" : 0, "std_date" : 0, "std_dow": 0, "std_hour": 0 },
+        { "name": "australia/perth",       "offset" : 480, "dst_month":  0, "dst_date":  0, "dst_dow": 0, "dst_hour": 0, "std_month" : 0, "std_date" : 0, "std_dow": 0, "std_hour": 0 },
+        { "name": "australia/adelaide",    "offset":  570, "dst_month": 10, "dst_date":  1, "dst_dow": 0, "dst_hour": 2, "std_month":  4, "std_date":  1, "std_dow": 0, "std_hour": 2 },
+        { "name": "australia/darwin",      "offset":  570, "dst_month":  0, "dst_date":  0, "dst_dow": 0, "dst_hour": 0, "std_month":  0, "std_date":  0, "std_dow": 0, "std_hour": 0 },
+        { "name": "australia/sydney",      "offset" : 600, "dst_month": 10, "dst_date":  1, "dst_dow": 0, "dst_hour": 2, "std_month" : 4, "std_date" : 1, "std_dow": 0, "std_hour": 2 },
+        { "name": "australia/brisbane",    "offset" : 600, "dst_month":  0, "dst_date":  0, "dst_dow": 0, "dst_hour": 0, "std_month" : 0, "std_date" : 0, "std_dow": 0, "std_hour": 0 },
+        { "name": "etc/utc",               "offset":    0, "dst_month":  0, "dst_date":  0, "dst_dow": 0, "dst_hour": 0, "std_month":  0, "std_date":  0, "std_dow": 0, "std_hour": 0 }
+    ]
+}
---
+++ tzserver.js
@@ -0,0 +1,41 @@
+const http = require("http");
+const tzInfo = require("./tzinfo");
+
+const host = '0.0.0.0';
+const port = 5000;
+
+const requestListener = function (req, res)
+{
+    var url = new URL("http://path.abc" + req.url);
+    tzName = url.searchParams.get("tzname");
+    if (tzName == undefined)
+    {
+        tzNamer = "etc/utc";
+    }
+
+    var entry = tzInfo.tzInfo.find(entry =>
+        entry.name.localeCompare(tzName, undefined,
+        { sensitivity: 'accent' }) === 0);
+
+    if (entry === undefined)
+    {
+        entry = tzInfo.tzInfo.find(entry => entry.name === "etc/utc");
+    }
+    console.log("Request: " + tzName);
+    console.log("Result:");
+    console.log(entry);
+    console.log("");
+
+    res.setHeader("Content-Type", "application/json");
+    res.writeHead(200);
+    res.end(JSON.stringify(entry));
+};
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () =>
+{
+    console.log(`Server is running on http://${host}:${port}`);
+});
+
+
+
